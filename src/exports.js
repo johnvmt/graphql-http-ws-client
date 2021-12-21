3acea208982bd08efb0ee6244179279ea7dacddb
John Murphy-Teixidor <johnvmt@gmail.com>
2021-12-21 17:48:14 -0500

    v2.0.2 - Added ApolloLink exports to package exports

--- src/exports.js
+++ src/exports.js
@@ -5,3 +5,4 @@ export default createGraphQLClient;
 export {createGraphQLClient, createGraphQLLinks};
 export * from "@apollo/client/cache/cache.cjs";
 export * from "@apollo/client/core/core.cjs";
+export * from "@apollo/client/link/core/core.cjs";
